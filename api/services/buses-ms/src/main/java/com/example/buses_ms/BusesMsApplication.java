1be2efec29bf79cc6ce4f884476d2ee185ff1043
ROGER MESTANZA <rogers.mestanza@gmail.com>
2025-05-23 17:15:37 -0500

    configurations completed successfuly and done to implement the mcroservice with id=buses-ms

--- api/services/buses-ms/src/main/java/com/example/buses_ms/BusesMsApplication.java
+++ api/services/buses-ms/src/main/java/com/example/buses_ms/BusesMsApplication.java
@@ -3,11 +3,15 @@ package com.example.buses_ms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class BusesMsApplication {
+	public static final Logger logger = Logger.getLogger(SpringApplication.class.toString());
 
 	public static void main(String[] args) {
 		SpringApplication.run(BusesMsApplication.class, args);
+		logger.info("Application started and running on port 8001");
 	}
 
 }
