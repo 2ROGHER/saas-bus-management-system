a4c94a0c76ba07510f85a1d4f920993ed8ec8d2a
ROGER MESTANZA <rogers.mestanza@gmail.com>
2025-05-24 16:11:19 -0500

    adding final settings files and more on on service with id=buses-ms

--- api/services/buses-ms/src/main/java/com/example/buses_ms/BusesMsApplication.java
+++ api/services/buses-ms/src/main/java/com/example/buses_ms/BusesMsApplication.java
@@ -1,5 +1,6 @@
 package com.example.buses_ms;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,8 +11,10 @@ public class BusesMsApplication {
 	public static final Logger logger = Logger.getLogger(SpringApplication.class.toString());
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
 		SpringApplication.run(BusesMsApplication.class, args);
-		logger.info("Application started and running on port 8001");
+		logger.info("Application started and running on port " + dotenv.get("SERVER_PORT"));
 	}
 
 }
